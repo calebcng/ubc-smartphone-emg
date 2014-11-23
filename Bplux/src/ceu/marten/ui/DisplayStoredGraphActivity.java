3dd684950a28da858e65b926e11a1116b12c887a
calebcng <calebcng+git@gmail.com>
2014-11-22 17:20:54 -0800

    DSGA class updated with a rough asynctask

--- Bplux/src/ceu/marten/ui/DisplayStoredGraphActivity.java
+++ Bplux/src/ceu/marten/ui/DisplayStoredGraphActivity.java
@@ -26,6 +26,8 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.view.Menu;
@@ -34,47 +36,11 @@ import android.app.Activity;
 
 public class DisplayStoredGraphActivity extends Activity {
 	private final Handler mHandler = new Handler();
-	private GraphView graphView;
-	private GraphViewSeries exampleSeries1;
 //	private ArrayList<Double> dataSet = new ArrayList<Double>();
 	private Vector<Double> dataSet = new Vector<Double>();
 	private static final File externalStorageDirectory = Environment.getExternalStorageDirectory();
 	String recordingName = "EMG_DATA";
 	String endOfHeader = "# EndOfHeader";
-  
-
-  int n = 0;
-  int max=0;
-  int min=0;
-  
-  public static double [] buffer;
-//  public static double [] buffer = {5, 7, 7, 1, 2, 8, 8, 4, 5, 5, 4};
-  
-  private double getRandom() {
-  	double number = buffer[n];
-  	if (number > max){
-  		max = (int)number;
-  	}
-  	else if (number < min){
-  		min = (int)number;
-  	}
-  	n++;
-  	return number;
-  }
-  
-  /*
-   * Calculates the range (min and max) of values of the dataSet vector
-   * Parameters:	none
-   * Outputs:	Double[2]; Double[0] = min, Double[1] = max
-   */
-  private double[] calculateRange() {
-  	double dataRange[] = {0, 0}; //{y-min, y-max}
-  	Object dataMin = Collections.min(dataSet);
-  	Object dataMax = Collections.max(dataSet);
-  	dataRange[0] = (double) dataMin;
-  	dataRange[1] = (double) dataMax;    	
-  	return dataRange;
-  }
   
   /*
    * Retrieves data points from specified data file
@@ -165,60 +131,7 @@ public class DisplayStoredGraphActivity extends Activity {
 	  	return true;
   }
   
-  private int calculateYScale() {
-	// Calculate range of y values
-	double yBounds[] = {0,0};
-	yBounds = calculateRange();
-	// Calculate the appropriate max value and min values in y-direction 
-	max = (int) yBounds[1] + 1;
-	if (yBounds[0] >= 0) {    
-	min = (int) yBounds[0];
-	}
-	else {
-		min = (int) yBounds[0] - 1;
-	}
-	
-	// Calculate interval level of labels in y-direction
-	int yInterval;
-	if ((max-min) <= 10) {
-		yInterval = 1;
-	}
-	else if ((max-min) <= 50) {
-		yInterval = 2;
-	}
-	else if ((max-min) <=100) {
-	  	yInterval = 5;
-	}
-	else if ((max-min) <= 200){
-	  	yInterval = 10;
-	}
-	else {
-	  	yInterval = 25;
-	}
-	
-	return yInterval;
-  }
-  
-  	private int calculateXScale() {
-  		int xInterval;
-  		int xMax = dataSet.size();
-  		if (xMax <= 10) {
-  			xInterval = 1;
-  		}
-  		else if (xMax <= 50) {
-  			xInterval = 5;
-  		}
-  		else if (xMax <=100) {
-  			xInterval = 10;
-  		}
-  		else {
-  			xInterval = 20;
-  		}
-  		return xInterval;
-  	}
-
-
-	@Override
+  	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		System.out.println("Initiating creation of DisplayStoredGraphActivity class");
 		Intent intent = getIntent();
@@ -230,16 +143,64 @@ public class DisplayStoredGraphActivity extends Activity {
 			System.out.println("Unable to retrieve FILE_NAME");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.display_stored_graph_activity);
-		Random randomGenerator = new Random();		
 		
-		if (!retrieveDataPoints(recordingName + Constants.TEXT_FILE_EXTENTION))
-		{
-			System.out.println("@IOERROR: Unable to read from file.");
-			for(int i=0; i<100; i++)
-		    {
-				dataSet.add(randomGenerator.nextDouble());
-		    }
+		LinearLayout layout = (LinearLayout) findViewById(R.id.dataGraph);
+		
+		ReadFileService readFile = new ReadFileService(this, layout);
+		readFile.execute(recordingName);
+		
+  }
+}
+
+
+//<Params, Progress, Result>
+class ReadFileService extends AsyncTask<String, String, Boolean> {
+	private GraphView graphView;
+	private GraphViewSeries exampleSeries1;
+	Vector<Double> dataSet = new Vector<Double>();
+	File externalStorageDirectory = Environment.getExternalStorageDirectory();
+	String recordingName = "EMG_DATA";	//Default filename
+	String endOfHeader = "# EndOfHeader";
+	int n = 0;
+	int max=0;
+	int min=0;		  
+	public static double [] buffer;
+	Context context;
+	LinearLayout layout;
+	static final int progress_bar_type = 0;
+	
+	public ReadFileService(Context context, LinearLayout layout) {
+		this.context = context;
+		this.layout = layout;
+	}
+		  
+	private double getRandom() {
+	double number = buffer[n];
+		if (number > max){
+			max = (int)number;
 		}
+		else if (number < min){
+			min = (int)number;
+		}
+		n++;
+		return number;
+	}
+	  
+	  /*
+	   * Calculates the range (min and max) of values of the dataSet vector
+	   * Parameters:	none
+	   * Outputs:	Double[2]; Double[0] = min, Double[1] = max
+	   */
+	private double[] calculateRange() {
+		double dataRange[] = {0, 0}; //{y-min, y-max}
+		Object dataMin = Collections.min(dataSet);
+		Object dataMax = Collections.max(dataSet);
+		dataRange[0] = (double) dataMin;
+		dataRange[1] = (double) dataMax;    	
+		return dataRange;
+	}
+	
+	private void graphData() {
 		
 		exampleSeries1 = new GraphViewSeries(new GraphViewData[] {
 	        });
@@ -252,7 +213,7 @@ public class DisplayStoredGraphActivity extends Activity {
 	  }
 	  
 	  System.out.println("Defining data set.");
-	  GraphView graphView = new LineGraphView(this, recordingName) {
+	  GraphView graphView = new LineGraphView(context, recordingName) {
 	  	   protected String formatLabel(double value, boolean isValueX) {
 	  	      // return as Integer
 	  	      return ""+((int) value);
@@ -284,15 +245,130 @@ public class DisplayStoredGraphActivity extends Activity {
 	  	graphView.setViewPort(0, 50);
 	  graphView.setManualYAxisBounds(yLabel, min);
 	  graphView.getGraphViewStyle().setNumVerticalLabels(((yLabel-min)/yInterval) + 1);
-//      graphView.getGraphViewStyle().setNumHorizontalLabels(xLabel/xInterval + 1);
-      graphView.getGraphViewStyle().setGridColor(Color.BLACK);
-      graphView.getGraphViewStyle().setHorizontalLabelsColor(Color.BLACK);
-      graphView.getGraphViewStyle().setVerticalLabelsColor(Color.BLACK);
-      graphView.getGraphViewStyle().setVerticalLabelsWidth(80);
+	//  graphView.getGraphViewStyle().setNumHorizontalLabels(xLabel/xInterval + 1);
+	  graphView.getGraphViewStyle().setGridColor(Color.BLACK);
+	  graphView.getGraphViewStyle().setHorizontalLabelsColor(Color.BLACK);
+	  graphView.getGraphViewStyle().setVerticalLabelsColor(Color.BLACK);
+	  graphView.getGraphViewStyle().setVerticalLabelsWidth(80);
 
-      LinearLayout layout = (LinearLayout) findViewById(R.id.dataGraph);
-      layout.addView(graphView);
-  }
+	  layout.addView(graphView);
+	}
+	
+	private int calculateYScale() {
+		// Calculate range of y values
+		double yBounds[] = {0,0};
+		yBounds = calculateRange();
+		// Calculate the appropriate max value and min values in y-direction 
+		max = (int) yBounds[1] + 1;
+		if (yBounds[0] >= 0) {    
+		min = (int) yBounds[0];
+		}
+		else {
+			min = (int) yBounds[0] - 1;
+		}		
+		// Calculate interval level of labels in y-direction
+		int yInterval;
+		if ((max-min) <= 10) {
+			yInterval = 1;
+		}
+		else if ((max-min) <= 50) {
+			yInterval = 2;
+		}
+		else if ((max-min) <=100) {
+			yInterval = 5;
+		}
+		else if ((max-min) <= 200){
+			yInterval = 10;
+		}
+		else {
+			yInterval = 25;
+		}
+		return yInterval;
+	}
+	  
+	private int calculateXScale() {
+		int xInterval;
+		int xMax = dataSet.size();
+		if (xMax <= 10) {
+			xInterval = 1;
+		}
+		else if (xMax <= 50) {
+			xInterval = 5;
+		}
+		else if (xMax <=100) {
+			xInterval = 10;
+		}
+		else {
+			xInterval = 20;
+		}
+		return xInterval;
+	}
+	
+	@Override
+	protected Boolean doInBackground(String... FILE_NAME) {		
+		Scanner strings = null;
+		recordingName = FILE_NAME[0];
+	  	try {
+	  		System.out.println(externalStorageDirectory + Constants.APP_DIRECTORY + recordingName + Constants.TEXT_FILE_EXTENTION);
+	  		File file = new File(externalStorageDirectory + Constants.APP_DIRECTORY, recordingName + Constants.TEXT_FILE_EXTENTION);
+	  		
+	  		if (file.exists()) {
+	  			FileReader read = new FileReader(file);
+	  			strings = new Scanner(read);
+	  			
+	      		strings.findWithinHorizon(endOfHeader,0);    		
+	      		strings.useDelimiter("\t *");
+	      		strings.next();
+	  		}
+		}
+		catch (FileNotFoundException error) {
+			System.out.println("@IOERROR: " + error);
+			return false;
+		}
+		catch (IOException error) {
+			System.out.println("@IOERROR: " + error);
+			return false;
+		}
+		while (strings.hasNext())
+		{
+			double dataPoint = Double.parseDouble(strings.next());
+			System.out.println("Adding " + dataPoint + " to vector.");
+			dataSet.add(dataPoint);
+			System.out.println("testData size: " + dataSet.size());
+			if (strings.hasNext())
+				strings.next();
+			else
+				break;
+		}
+		System.out.println("Closing strings.");
+	  	strings.close();
+		
+		return true;
+	}
+	
+	protected void onProgressUpdate() {
+		//called when the background task makes any progress
+	}
+
+	protected void onPreExecute() {
+/*		//called before doInBackground() is started
+		super.onPreExecute();
+		// Show Progress Bar Dialog before calling doInBackground method
+		showDialog(progress_bar_type);*/
+	}
+	
+	protected void onPostExecute(Boolean readFileSuccess) {
+		//called after doInBackground() has finished 
+		if(!readFileSuccess) {
+			Random randomGenerator = new Random();			
+			System.out.println("@IOERROR: Unable to read from file. Creating random dataset");
+			for(int i=0; i<100; i++)
+		    {
+				dataSet.add(randomGenerator.nextDouble());
+		    }
+		}
+		
+		this.graphData();
+	}
 }
-
 
